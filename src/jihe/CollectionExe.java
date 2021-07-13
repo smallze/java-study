ee1fd89cff41ce0873638e49c7dfb517d91c0e0d
xiaoze <1067632338@qq.com>
2021-07-13 23:57:52 +0800

    collection util

--- src/jihe/CollectionExe.java
+++ src/jihe/CollectionExe.java
@@ -1,9 +1,6 @@
 package jihe;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author zzs
@@ -28,9 +25,36 @@ public class CollectionExe {
         System.out.println("reverse 反转" + list);
         System.out.println("max " + Collections.max(list));
         System.out.println("min " + Collections.min(list));
-        Collections.swap(list,0,1);
-        System.out.println("swap 交换0和1 位置 "+list);
-        Collections.fill(list,100);
-        System.out.println("fill 100 "+list);
+        Collections.swap(list, 0, 1);
+        System.out.println("swap 交换0和1 位置 " + list);
+        Collections.fill(list, 100);
+        System.out.println("fill 100 " + list);
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("pg");
+        list1.add("sg");
+        list1.add("sf");
+        list1.add("pf");
+        list1.add("pf");
+        list1.add("c");
+        System.out.println(list1);
+        Collections.sort(list1, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+        System.out.println("collection sort " + list1);
+        System.out.println(Collections.max(list1));
+        System.out.println(Collections.min(list1));
+        System.out.println(Collections.frequency(list1, "pf"));
+        List<String> copyArr = new ArrayList<>();
+        for (int i = 0; i < list1.size(); i++) {
+            copyArr.add("");
+        }
+        Collections.copy(copyArr, list1);
+        System.out.println("copy " + copyArr);
+        Collections.replaceAll(list1,"pf","changed pf");
+        System.out.println(list1);
     }
 }
